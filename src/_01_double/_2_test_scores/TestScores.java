0e0a8510bf023c41533373df066a5f9a034c1d89
root <root@45a5f61ce0d2>
2022-04-14 23:56:46 +0000

    i worked on testscores

--- src/_01_double/_2_test_scores/TestScores.java
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -6,7 +6,38 @@ public class TestScores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-JOptionPane.showInputDialog("what is your most recent test score?")
+String answer = JOptionPane.showInputDialog("what is your most recent test score?");
+double answerdouble = Double.parseDouble(answer);
+if(answerdouble == 100) {
+
+JOptionPane.showMessageDialog(null, "wow. very school.");
+
+}
+else if(answerdouble > 90) {
+
+JOptionPane.showMessageDialog(null,"noice.");
+
+}
+else if(answerdouble > 60) {
+
+JOptionPane.showMessageDialog(null,"pretty good.");
+
+}
+else if(answerdouble > 50) {
+
+JOptionPane.showMessageDialog(null,"ok.");
+
+}
+else if(answerdouble > ) {
+
+JOptionPane.showMessageDialog(null,"noice.");
+
+}
+else {
+syso("You got 10 or less");
+}
+
 	}
+
 
 }
