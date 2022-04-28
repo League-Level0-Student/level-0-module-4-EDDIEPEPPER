f9aa25ec040368b0a980eacee5664bf460dd0a2c
root <root@e2e4f8ce2750>
2022-04-28 23:47:18 +0000

    i did game over sleepy head star student and red square

--- src/_02_boolean/_3_game_over/GameOver.java
+++ src/_02_boolean/_3_game_over/GameOver.java
@@ -9,12 +9,19 @@ public class GameOver {
 		boolean gameOver = false;
 		
 		// 1. Ask the user if the game is over and save their answer.
-		
+		while(!gameOver) {
+		String gameoOver= JOptionPane.showInputDialog("is the game over.");
 		// 2. If they answer "yes" change gameOver to true.
-		
+		if(gameoOver.equalsIgnoreCase("yes")) {
+			gameOver=true;
+		}
+		else if(gameoOver.equalsIgnoreCase("no")) {
+			gameOver=false;
+		}
 		// 3. Surround steps 1-2 in a while loop that runs as long as
 		//    gameOver is false. Hint: Use the ! operator.
-		
+		}
+		JOptionPane.showMessageDialog(null, "the game is over");
 		// 4. Tell the user the game is over outside the while loop.
 		
 	}
